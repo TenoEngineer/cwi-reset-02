3eb6222c1e46a711230aa151ec67198e6bb8715a
Heitor Teno Müller <32706115+TenoEngineer@users.noreply.github.com>
2021-02-16 06:10:00 -0300

    Finalização exercícios aula 02

--- aula-2/exercicios-aula-02/src/Filme.java
+++ aula-2/exercicios-aula-02/src/Filme.java
@@ -6,12 +6,15 @@ public class Filme {
     private int avaliacao;
     private Diretor diretor;
 
+    private
+
     public Filme(String nome, String descricao, double duracao, int anoLancamento, int avaliacao, Diretor diretor) {
+        defineAvaliacao(avaliacao);
+        validaNomeEDefineAvalicao(nome);
         this.nome = nome;
         this.descricao = descricao;
         this.duracao = duracao;
         this.anoLancamento = anoLancamento;
-        this.avaliacao = avaliacao;
         this.diretor = diretor;
     }
 
@@ -21,4 +24,21 @@ public class Filme {
         System.out.println("Duração: " + this.duracao);
         System.out.println("Nome do Diretor: " + this.diretor.getNome());
     }
+
+    private void defineAvaliacao(int avaliacao) {
+        if (avaliacao < 1 || avaliacao > 5) {
+            this.avaliacao = 3;
+        } else {
+            this.avaliacao = avaliacao;
+        }
+    }
+
+    private void validaNomeEDefineAvalicao(String nome) {
+        if ("Batman vs Superman".equals(nome)) {
+            this.avaliacao = 1;
+        } else if ("Interstelar".equals(nome)){
+            this.avaliacao = 5;
+        }
+    }
+
 }
